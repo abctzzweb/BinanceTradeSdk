33dc17f9e1d350ec67f6318c11d377d2d72ab1c3
abctzz <abctzz@outlook.com>
2025-07-19 12:39:29 +0800

    first commit

--- src/main/java/org/binance/trade/rep/AccountBalanceRep.java
+++ src/main/java/org/binance/trade/rep/AccountBalanceRep.java
@@ -5,7 +5,9 @@ import org.binance.trade.BinanceSDKConts;
 import org.binance.trade.rep.ben.AccountBalanceBen;
 import org.binance.trade.req.AccountBalanceReq;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class AccountBalanceRep extends BaseResponse<AccountBalanceReq> {
 
@@ -24,13 +26,29 @@ public class AccountBalanceRep extends BaseResponse<AccountBalanceReq> {
         this.data = data;
     }
 
+    /**
+     * 获取USDT可用余额
+     */
     public String getUsdtAvailableBalance() {
-        for (int i = 0; i < this.data.size(); i++) {
-            AccountBalanceBen accountBalanceBen =  this.data.get(i);
-            if(BinanceSDKConts.USDT.equals(accountBalanceBen.getAsset())){
-                return accountBalanceBen.getAvailableBalance();
-            }
-        }
-        return "0.0";
+        return Optional.ofNullable(data)
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(balance -> BinanceSDKConts.USDT.equals(balance.getAsset()))
+                .findFirst()
+                .map(AccountBalanceBen::getAvailableBalance)
+                .orElse("0.0");
+    }
+
+    /**
+     * 获取指定资产的可用余额 - 通用方法
+     */
+    public String getAssetAvailableBalance(String asset) {
+        return Optional.ofNullable(data)
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(balance -> asset.equals(balance.getAsset()))
+                .findFirst()
+                .map(AccountBalanceBen::getAvailableBalance)
+                .orElse("0.0");
     }
 }
